58118f34a9d41b74f6a3dc4c3aac2c1423d6326e
gukeyboi <lla.kowalik.lain@gmail.com>
2026-02-04 15:18:09 +0100

    commit glitch, mosaic, mosaic mirror et autres

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,20 @@ void mirror(sil::Image &image) {
 }
 }
 
+void mirror_vertical(sil::Image &image) {
+    for (int x{0}; x < image.width(); x++)
+{
+    for (int y{0}; y < image.height() / 2; y++)
+    {
+
+        std::swap(image.pixel(x, y).r, image.pixel(x,image.height()-y-1).r);
+        std::swap(image.pixel(x, y).b, image.pixel(x,image.height()-y-1).b);
+        std::swap(image.pixel(x, y).g, image.pixel(x,image.height()-y-1).g);
+        
+    }
+}
+}
+
 void invert_colors(sil::Image &image) {
     for (int x{0}; x < image.width(); x++)
 {
@@ -157,6 +171,49 @@ void noise(sil::Image &image) {
 }
 }
 
+void mosaic(sil::Image &image) {
+    sil::Image newImage(image.width()*5, image.height()*5);
+
+    for (int x{0}; x < image.width()*5; x++)
+{
+    for (int y{0}; y < image.height()*5; y++)
+    {
+       newImage.pixel(x, y).r = image.pixel(x%image.width(), y%image.height()).r;
+       newImage.pixel(x, y).g = image.pixel(x%image.width(), y%image.height()).g;
+       newImage.pixel(x, y).b = image.pixel(x%image.width(), y%image.height()).b;
+
+    }
+}
+ newImage.save("output/pouet.png");
+}
+
+void mirrormosaic(sil::Image &image) {
+    sil::Image newImage(image.width()*5, image.height()*5);
+    sil::Image alsonewImage = image;
+    int nb_iterations = 9;
+   // for (int neuille{0}; neuille < nb_iterations; neuille++) 
+   for (int j{0}; j < 5; j++){ 
+   
+        for (int i{0}; i < 5; i++){
+
+            for (int x{0}; x < image.width(); x++)
+            { 
+                for (int y{0}; y < image.height(); y++)
+                {
+                newImage.pixel(x+ (image.width()*i), y+ (image.height()*j)).r = image.pixel(x , y).r;
+                newImage.pixel(x+ (image.width()*i), y+ (image.height()*j)).g = image.pixel(x , y).g;
+                newImage.pixel(x+ (image.width()*i), y+ (image.height()*j)).b = image.pixel(x , y).b;
+                }
+            }
+        mirror(image);
+        }
+        image = alsonewImage;
+        mirror_vertical(image);
+        alsonewImage = image ;
+}
+newImage.save("output/pouet.png");
+}
+
 void gradient(sil::Image &image) {
     for (int x{0}; x < image.width(); x++)
 {
@@ -169,13 +226,38 @@ void gradient(sil::Image &image) {
 }
 }
 
+void glitch(sil::Image &image) {
+    int nb_iterations_2 = random_int(10, 30);
+
+    
+    for (int i{0}; i < nb_iterations_2; i++) {
+        
+        int glitch_width = random_int(100, 160);
+        int glitch_height = random_int(140, 160);
+        int swapper_x = random_int(0, image.width() - glitch_width);
+        int swapper_y = random_int(0, image.height() - glitch_height);
+        int rect_x = random_int(0, image.width() - glitch_width);
+        int rect_y = random_int(0, image.height() - glitch_height);
+
+
+    for (int x{0}; x < glitch_width; x++) {
+        for (int y{0}; y < glitch_height; y++)
+    {
+        std::swap(image.pixel(rect_x + x, rect_y + y), image.pixel(swapper_x + x , swapper_y + y));
+    }
+}    
+
+}
+}
+
 int main()
 {
     sil::Image image{"images/logo.png"};
     // TODO: modifier l'image
     //sil::Image image{300/*width*/, 200/*height*/};
-    brightness(image);
-    image.save("output/pouet.png");
+    glitch(image);
+   image.save("output/pouet.png");
+   
 }
 
 
